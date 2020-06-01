77ad6c089004397989121bb14a44e8828601c248
Saurabh Daware <saurabhdaware99@gmail.com>
2020-06-01 15:48:28 +0530

    refactored to use static-main for main js code

--- theme/js/static-main.js
+++ theme/js/static-main.js
@@ -1,2 +1,7 @@
 // When important links are done loading, we show the content.
-hljs.initHighlightingOnLoad();
+if (hljs) {
+  hljs.initHighlightingOnLoad();
+}
+    
+window.addEventListener('keydown', e => e.keyCode === 9 && (document.body.classList.add("user-is-tabbing")));
+document.querySelector('#footer-year').innerHTML = new Date().getFullYear();
