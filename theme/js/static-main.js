de426a9d4253185121bc13c46253a0ae56146ea0
Saurabh Daware <saurabhdaware99@gmail.com>
2020-06-01 16:42:45 +0530

    static-main.js not linked in index fix

--- theme/js/static-main.js
+++ theme/js/static-main.js
@@ -1,7 +1,5 @@
-// When important links are done loading, we show the content.
-if (hljs) {
-  hljs.initHighlightingOnLoad();
-}
-    
-window.addEventListener('keydown', e => e.keyCode === 9 && (document.body.classList.add("user-is-tabbing")));
+    window.addEventListener('keydown', e => e.keyCode === 9 && (document.body.classList.add("user-is-tabbing")));
 document.querySelector('#footer-year').innerHTML = new Date().getFullYear();
+try {
+  hljs.initHighlightingOnLoad();
+} catch(err) {}
