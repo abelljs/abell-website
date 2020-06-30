8ea356d96be70427f202a97baa47577da9e4baec
Saurabh Daware <saurabhdaware99@gmail.com>
2020-06-30 19:38:04 +0530

    navigation and links added to playground

--- theme/components/GitHubContributeButton.js
+++ theme/components/GitHubContributeButton.js
@@ -1,10 +1,10 @@
-function GitHubContributeButton(navigationPath) {
+function GitHubContributeButton(navigationPath, isFullPath = false) {
   return /* html */`
   <a 
     target="_blank" 
     rel="noopener noreferer" 
     class="github-contribute-button blog dynamic-url shadow" 
-    href="https://github.com/abelljs/abell-website/tree/main/content/${navigationPath}" 
+    href="${isFullPath ? isFullPath : `https://github.com/abelljs/abell-website/tree/main/content/${navigationPath}`}" 
     data-size="large"
   >
     <img width="20" style="position: relative;top:4px;" alt="GitHub Logo" src="../icons/github.png"> 
--- theme/js/code-run.js
+++ theme/js/code-run.js
@@ -1,5 +1,6 @@
-document.querySelector('#run-code').addEventListener('click', e => {
+function setOutput() {
   const codeToExecute = document.querySelector('#code-to-run').innerText;
+  document.querySelector('#html-output').innerText = 'Executing code...';
   fetch('https:///abell-renderer-api.netlify.app/.netlify/functions/renderer', {
     method: 'POST',
     body: codeToExecute
@@ -7,5 +8,9 @@ document.querySelector('#run-code').addEventListener('click', e => {
     .then(res => res.text())
     .then(res => {
       document.querySelector('#html-output').innerText = res.replace(/\n\n/g, '\n');
+      hljs.highlightBlock(document.querySelector('#html-output'));
     })
-})
+}
+
+document.querySelector('#run-code').addEventListener('click', setOutput);
+setOutput();
