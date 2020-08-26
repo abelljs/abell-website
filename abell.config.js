a917b347858a65672ca290df015665ca11de623f
Saurabh Daware <saurabhdaware99@gmail.com>
2020-08-26 16:33:25 +0530

    v bump to 0.5

--- abell.config.js
+++ abell.config.js
@@ -2,6 +2,7 @@ module.exports = {
   globalMeta: {
     siteName: 'Abell',
     name: 'Hoomans of Abell Land',
+    version: '0.5',
     twitter: 'AbellLand',
     domain: 'https://abelljs.org/',
     navMenuOrder: [
@@ -12,34 +13,6 @@ module.exports = {
       'plugins',
       'contributing',
       'changelog'
-    ],
-    builtWithAbell: [
-      {
-        title: 'Abelljs.org (This documentation website) 🎉',
-        description: 'The content in this website is written inside markdown and json files and Abell applies this content over a layout to build this pretty website 🐨 🎉',
-        url: 'https://abelljs.org'
-      },
-      {
-        title: 'Akashic Records',
-        description: 'Blog of Akash',
-        url: 'https://records.akashj.com/'
-      },
-      {
-        title: 'React Native Wifi and Hotspot Library Documentation',
-        description: 'A React Native Library To Configure Wifi & Hotspot Settings',
-        url: 'https://react-native-wifi-and-hotspot-wizard.netlify.app/'
-      },
-      {
-        title: 'Unlighted',
-        description: 'Astronomy blog of Saurabh (Not ready yet)',
-        url: 'https://unlighted.saurabhdaware.in'
-      },
-      {
-        title: 'Portfolio - Saurabh',
-        description: 'Portfolio website that reads projects, articles, etc from JSON and builds website',
-        url: 'https://saurabhdaware.in'
-      }
-      
     ],
     themes: [
       {
