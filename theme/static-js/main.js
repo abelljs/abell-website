b130289646d19db9c172201bc020dfa1228b6113
saurabhdaware <saurabhdaware99@gmail.com>
2020-11-29 16:08:57 +0530

    Add Light Mode

---
+++ theme/static-js/main.js
@@ -0,0 +1,25 @@
+if (hljs) {  
+  function abellHighlighter(e) {
+    return {
+      name: "abell",
+      aliases: ["Abell", "abell"],
+      subLanguage: ["xml"],
+      contains: [
+        e.C_LINE_COMMENT_MODE,
+        e.C_BLOCK_COMMENT_MODE,
+        {
+          className: "abell",
+          begin: "{{",
+          end: "}}",
+          subLanguage: ["javascript"]
+        }
+      ],
+    };
+  }
+
+  hljs.registerLanguage('abell', abellHighlighter);
+  hljs.initHighlightingOnLoad();
+}
+
+// check if user is tabbing (a11y)
+window.addEventListener('keydown', e => e.keyCode === 9 && (document.body.classList.add("user-is-tabbing")));
