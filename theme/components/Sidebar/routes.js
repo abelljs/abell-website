ac35518c2ddca094cab2274018767653ea303998
saurabhdaware <saurabhdaware99@gmail.com>
2020-12-13 22:39:30 +0530

    add routes

---
+++ theme/components/Sidebar/routes.js
@@ -0,0 +1,10 @@
+const routes = {
+  'Getting Started': '/getting-started/',
+  'Installation': '/installation/',
+  // 'Abell Starters': '/starters/',
+  'Tutorial': '/tutorial/create-portfolio/',
+  'Syntax Guide': '/guide/syntax-guide/',
+  'API Reference': '/guide/api-reference/'
+}
+
+module.exports = routes;
