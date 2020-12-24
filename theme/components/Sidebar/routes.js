7342a39ae7dead20ec04ea5e9f24b4f6801085c0
saurabhdaware <saurabhdaware99@gmail.com>
2020-12-24 23:18:23 +0530

    create sub-menu in sidebar

--- theme/components/Sidebar/routes.js
+++ theme/components/Sidebar/routes.js
@@ -2,9 +2,15 @@ const routes = {
   'Getting Started': '/getting-started/',
   'Installation': '/installation/',
   // 'Abell Starters': '/starters/',
-  'Tutorial': '/tutorial/create-portfolio/',
+  'Tutorial': {
+    path: '/tutorial/create-portfolio/',
+    routes: {
+      'What are we building': '/tutorial/create-portfolio/#what-are-we-building',
+      'Prerequisites': '/tutorial/create-portfolio/#prerequisites'
+    }
+  },
   'Syntax Guide': '/guide/syntax-guide/',
-  'API Reference': '/guide/api-reference/'
+  'API Reference': '/guide/api-reference/',
 }
 
 module.exports = routes;
