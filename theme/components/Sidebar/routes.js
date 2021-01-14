a9fa706f2d31b906eb77ccb2b2e6b5fb492c9092
saurabhdaware <saurabhdaware99@gmail.com>
2021-01-14 22:57:37 +0530

    fix(Sidebar): fix tutorial paths

--- theme/components/Sidebar/routes.js
+++ theme/components/Sidebar/routes.js
@@ -2,15 +2,15 @@ const routes = {
   'Getting Started': '/getting-started/',
   'Abell Starters': '/starters/',
   'Tutorial': {
-    path: '/tutorial/create-portfolio/',
+    path: '/tutorial/',
     routes: {
-      'What is Abell?': '/tutorial/create-portfolio/#what-is-abell',
-      'Installation and Setup': '/tutorial/create-portfolio/#installation-and-setup',
-      'Setting Up Our Portfolio': '/tutorial/create-portfolio/#setting-up-our-portfolio',
-      'Adding CSS to Our Project': '/tutorial/create-portfolio/#adding-css-to-our-project',
-      'Require JSON to Abell Files': '/tutorial/create-portfolio/#require-json-to-abell-files',
-      'Building Your First Abell Component': '/tutorial/create-portfolio/#building-your-first-abell-component',
-      'Dynamic Page Generation': '/tutorial/create-portfolio/#dynamic-page-generation',
+      'What is Abell?': '/tutorial/#what-is-abell',
+      'Installation and Setup': '/tutorial/#installation-and-setup',
+      'Setting Up Our Portfolio': '/tutorial/#setting-up-our-portfolio',
+      'Adding CSS to Our Project': '/tutorial/#adding-css-to-our-project',
+      'Require JSON to Abell Files': '/tutorial/#require-json-to-abell-files',
+      'Building Your First Abell Component': '/tutorial/#building-your-first-abell-component',
+      'Dynamic Page Generation': '/tutorial/#dynamic-page-generation',
     }
   },
   'Syntax Guide': '/guide/syntax-guide/',
