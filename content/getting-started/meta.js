e8955fd727c6ced0555fec80a4b972278b341641
saurabhdaware <saurabhdaware99@gmail.com>
2021-01-23 17:10:10 +0530

    fix variable not defined error

--- content/getting-started/meta.js
+++ content/getting-started/meta.js
@@ -1,8 +1,5 @@
 module.exports = {
   title: "Getting Started",
   description: "This section is an overview of Abell Documentation and will help you navigate to the page you need.",
-  $createdAt: "Jun 5 2020",
-  components: {
-    Themes
-  }
+  $createdAt: "Jun 5 2020"
 }
