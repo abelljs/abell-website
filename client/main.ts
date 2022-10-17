52473e9d22a8c3f0429701ae1bba440d947afc6a
saurabhdaware <saurabhdaware99@gmail.com>
2022-10-17 23:34:55 +0530

    feat: add routing section

--- client/main.ts
+++ client/main.ts
@@ -1,14 +1,18 @@
 import '@fontsource/inter/variable.css';
 import 'highlight.js/styles/github.css';
 
-const editorFileExplorerButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.editor li button');
+const editors: NodeListOf<HTMLDivElement> = document.querySelectorAll('.editor');
+editors.forEach((editor) => {
+  const editorFileExplorerButtons: NodeListOf<HTMLButtonElement> = editor.querySelectorAll('li button');
 
-editorFileExplorerButtons.forEach((fileExplorerButton) => {
-  fileExplorerButton.addEventListener('click', () => {
-    const filename = fileExplorerButton.dataset.filename;
-    document.querySelector('.editor li button.active')?.classList.remove('active');
-    fileExplorerButton.classList.add('active');
-    document.querySelector('.editor .code-display div.show')?.classList.remove('show');
-    document.querySelector(`.editor .code-display div.file-${filename}`)?.classList.add('show');
+  editorFileExplorerButtons.forEach((fileExplorerButton) => {
+    fileExplorerButton.addEventListener('click', () => {
+      const filename = fileExplorerButton.dataset.filename;
+      editor.querySelector('li button.active')?.classList.remove('active');
+      fileExplorerButton.classList.add('active');
+      editor.querySelector('.code-display div.show')?.classList.remove('show');
+      editor.querySelector(`.code-display div.file-${filename}`)?.classList.add('show');
+    })
   })
+
 })
