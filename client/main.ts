f9eb507d316c54e41b04999f76aa61dda9a65177
saurabhdaware <saurabhdaware99@gmail.com>
2022-10-17 16:17:00 +0530

    feat: add initial editor component

--- client/main.ts
+++ client/main.ts
@@ -1,3 +1,14 @@
 import '@fontsource/inter/variable.css';
+import 'highlight.js/styles/github.css';
 
-console.log('main.ts');
+const editorFileExplorerButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.editor li button');
+
+editorFileExplorerButtons.forEach((fileExplorerButton) => {
+  fileExplorerButton.addEventListener('click', () => {
+    const filename = fileExplorerButton.dataset.filename;
+    document.querySelector('.editor li button.active')?.classList.remove('active');
+    fileExplorerButton.classList.add('active');
+    document.querySelector('.editor .code-display div.show')?.classList.remove('show');
+    document.querySelector(`.editor .code-display div.file-${filename}`)?.classList.add('show');
+  })
+})
